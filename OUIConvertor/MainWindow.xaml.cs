8fc57cb6f56475ebce238a501c4ae36b43326420
Kwang Beom Kim <knoxxr@gmail.com>
2022-02-01 00:33:52 +0900

    add project to geathering oui info to database

---
+++ OUIConvertor/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OUIConvertor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WebOUIConvertor ouiconverter = new WebOUIConvertor();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ouiconverter.ProgressChanged += Ouiconverter_ProgressChanged;
+            ouiconverter.ProgressCompleted += Ouiconverter_ProgressCompleted;
+        }
+
+        private void Ouiconverter_ProgressCompleted(int value)
+        {
+            ProgBar.Value = 0;
+            lbProgress.Content = string.Format("완료 : {0}", value);
+        }
+
+        private void Ouiconverter_ProgressChanged(int value)
+        {
+            ProgBar.Value = value;
+            lbProgress.Content = value.ToString();  
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ouiconverter.Initialize();
+        }
+    }
+}
---
+++ OUIConvertor/OUIConvertor.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OUIConvertor
+{
+    internal class WebOUIConvertor
+    {
+        public delegate void deleProgressChanged(int value);
+        public event deleProgressChanged ProgressChanged;
+
+        public delegate void deleProgressCompleted(int value);
+        public event deleProgressCompleted ProgressCompleted;
+
+        List<OUIInfo> OUTInfors = new List<OUIInfo>();
+        string ConnectString = @"220.94.240.240:33060";
+
+        string URLPath = @"http://standards-oui.ieee.org/oui/oui.txt";
+
+        WebClient client = new WebClient();
+
+        public struct OUIInfo
+        {
+            public string Mac;
+            public string Organization;
+            public string ComponyID;
+            public string Compony_Organization;
+            public string Address;
+            public string Contury;
+        }
+
+        public WebOUIConvertor()
+        {
+            client.DownloadProgressChanged += Client_DownloadProgressChanged;
+            client.DownloadFileCompleted += Client_DownloadFileCompleted;
+        }
+
+        private void Client_DownloadFileCompleted(object? sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (ProgressCompleted!= null)
+            {
+                ProgressCompleted(OUTInfors.Count);
+            }
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            if(ProgressChanged!= null)
+            {
+                ProgressChanged(e.ProgressPercentage);
+            }
+        }
+
+        public void Initialize()
+        {
+            GetRawOUI();
+        }
+        public void GetRawOUI()
+        {
+            OUTInfors.Clear();
+            Stream stream = client.OpenRead(URLPath);
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                while (reader.EndOfStream == false)
+                {
+                    string readText = reader.ReadLine();
+
+                    if (string.IsNullOrEmpty(readText)) continue;
+                    if (readText.Contains("(hex)"))
+                    {
+                        string oui_organ = readText;
+                        string company_organ = reader.ReadLine();
+                        string addr = reader.ReadLine() + reader.ReadLine();
+                        string contury = reader.ReadLine();
+                        AddOUIInfo(oui_organ, company_organ, addr, contury);
+                    }
+                }
+            }
+            if (ProgressCompleted != null)
+            {
+                ProgressCompleted(OUTInfors.Count);
+            }
+        }
+
+        public void AddOUIInfo(string oui_organ, string company_organ, string addr, string contury)
+        {
+            OUIInfo info = new OUIInfo();
+
+            string[] tokens_01 = oui_organ.Split("(hex)");
+
+            info.Mac = tokens_01[0].Trim();
+            info.Organization = tokens_01[1].Trim();
+
+            string[] tokens_02 = company_organ.Split("(base 16)");
+            info.ComponyID = tokens_02[0].Trim();
+            info.Compony_Organization = tokens_02[1].Trim();
+
+            info.Address = addr.Replace("\t\t\t\t", " ").Trim();
+            info.Contury = contury.Trim();
+
+            OUTInfors.Add(info);
+        }
+
+        public void UpdateDatabase()
+        {
+
+        }
+    }
+}
